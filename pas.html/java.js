2d31bb47dfcd04fc653e4b5f317790657f859e03
andikanteng <147516292+andikanteng@users.noreply.github.com>
2024-11-28 22:18:33 +0700

    Add files via upload

---
+++ pas.html/java.js
@@ -0,0 +1,14 @@
+
+const emailInput = document.getElementById('email');
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+    emailInput.value = '';
+});
+document.getElementById("button").addEventListenert("click", function(){
+window.open("profil.html", "_blank")
+})
+
+
+
+
+
